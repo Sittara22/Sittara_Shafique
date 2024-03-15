710b25e28895fb7637e48d2a4e868b1fc0a57cc3
Sittara22 <sittarashafique33@gmail.com>
2024-03-15 03:10:53 +0000

    flexbox fixed in About section

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,29 +1,75 @@
 import React from "react";
 import "./about.css";
-import js from "../../assets/js.png"
-import CSS from "../../assets/css3-alt.png"
-import HTML from "../../assets/html5.png"
-import react from "../../assets/react.png"
-import git from "../../assets/git-alt.png"
+import js from "../../assets/js.png";
+import CSS from "../../assets/css3-alt.png";
+import HTML from "../../assets/html5.png";
+import react from "../../assets/react.png";
+import git from "../../assets/git-alt.png";
 function About() {
   return (
     <div>
       <section id="about">
         <div className="aboutMe">
-          <span className="title">What I do?</span><br />
-          <span className="introDesc">Hello,My name is <span className="focu">Sittara Shafique</span>, and I currently reside in London. I did my <span className="focus">Master's degree in computer science in 2018</span> and worked as a <span className="focus">Computer Science Lecturer from 2018 to 2021.</span> I then pursued a second <span className="focus">Master's degree in project management from the University of the West of Scotland's London campus in 2021–2022</span>, immediately after which I learned new web development concepts. </span>
-         <br /> <span className="introDesc">Hi,here are the set of skills that helps me to develope a website. </span>
+          <span className="title">What I do?</span>
+          <br />
+          <span className="introDesc">
+            Hello,My name is <span className="focu">Sittara Shafique</span>, and
+            I currently reside in London. I did my{" "}
+            <span className="focus">
+              Master's degree in computer science in 2018
+            </span>{" "}
+            and worked as a{" "}
+            <span className="focus">
+              Computer Science Lecturer from 2018 to 2021.
+            </span>{" "}
+            I then pursued a second{" "}
+            <span className="focus">
+              Master's degree in project management from the University of the
+              West of Scotland's London campus in 2021–2022
+            </span>
+            , immediately after which I learned new web development concepts.{" "}
+          </span>
+          <br />{" "}
+          <span className="introDesc">
+            Hi,here are the set of skills that helps me to develope a website.{" "}
+          </span>
           <div className="skillsIcon">
-            <span className="Js imgFrame"><img src={js} alt="" className="Icon" /><h2 className="text">Javascript</h2></span>
-            <span className="CSS imgFrame"><img src={CSS} alt="" className="Icon" /><h2 className="text">CSS</h2></span>
-            <span className="HTML imgFrame"><img src={HTML} alt="" className="Icon" /><h2 className="text">HTML</h2></span>
-            <span className="react imgFrame"><img src={react} alt="" className="Icon" /><h2 className="text">React</h2></span>
-            <span className="git imgFrame"><img src={git} alt="" className="Icon" /><h2 className="text">Git</h2></span>
+            <div>
+              <span className="imgFrame">
+                <img src={js} alt="" className="Icon" />
+              </span>
+              <h2 className="text">Javascript</h2>
+            </div>
+            <div>
+              {" "}
+              <span className="imgFrame">
+                <img src={CSS} alt="" className="Icon" />
+              </span>
+              <h2 className="text">CSS</h2>
+            </div>
+            <div>
+              <span className="imgFrame">
+                <img src={HTML} alt="" className="Icon" />
+              </span>
+              <h2 className="text">HTML</h2>
+            </div>
+            <div>
+              <span className="imgFrame">
+                <img src={react} alt="" className="Icon" />
+              </span>
+              <h2 className="text">React</h2>
+            </div>
+            <div>
+              <span className="imgFrame">
+                <img src={git} alt="" className="Icon" />
+              </span>
+              <h2 className="text">Git</h2>
+            </div>
           </div>
         </div>
       </section>
     </div>
-  )
+  );
 }
 
 export default About;
--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -15,7 +15,13 @@ function Intro() {
             <p className="introPara">I am a skilled web designer who creates visually appealing and user friendly wesbsites.I enjoy writing code, and can at least translate my ideas into code :-).<span className="skillPara2">I know how to use <span className="lite">Python</span> and <span className="lite">C++</span> and I am fairly proficient in <span className="strong">JavaScript</span>.<br/>
         My field of interest is to develope websites by applying JavaScript Library<span className="strong"> React Js</span></span> </p>
         </div>
-            <Link><button onClick={()=>{document.getElementById('Contact_me').scrollIntoView({behavior:'smooth'})}} className="btn"><img className='btnImg' src={btnImg} alt="hire me" />Hire me</button></Link>            
+            <Link><button className="btn">
+  <a href="/resume/Sittara_Shafique.pdf" download>
+    <img className='btnImg' src={btnImg} alt="Hire me" />
+    <span>Hire me</span>
+  </a>
+</button>
+</Link>            
         </div>
     </section>
   )
