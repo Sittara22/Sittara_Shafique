9602168e34bfab54d760134fd1148a6302ef7404
Sittara22 <sittarashafique33@gmail.com>
2024-03-18 20:52:59 +0000

    dependencies added

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -14,9 +14,9 @@ function Works() {
             I take pride in paying attention to the smallest details and making sure that my work is pixel perfect.I am excited to bring my skills and experience to help business achieve their goals and create a stron online presence.
             </span>
             <div className="workImgs">
-            <div className='pFrame'><a href="https://steady-monstera-91b0eb.netlify.app/" target="_blank"><img src={musicApp} alt="" className="workImg"/></a><span className='pTitle'>Music App</span></div>
-            <div className='pFrame'><a href="https://resilient-flan-f09e1c.netlify.app/" target="_blank"><img src={currencyApp} alt="" className="workImg"/></a><span className='pTitle'>Currency Converter</span></div>
-            <div className='pFrame'> <a href="https://visionary-cajeta-31f4df.netlify.app/" target="_blank"><img src={todoApp} alt="" className="workImg"/></a><span className='pTitle'>Todo App</span></div>
+            <div className='pFrame'><a href="https://steady-monstera-91b0eb.netlify.app/" target="_blank" rel="noreferrer"><img src={musicApp} alt="" className="workImg"/></a><span className='pTitle'>Music App</span></div>
+            <div className='pFrame'><a href="https://resilient-flan-f09e1c.netlify.app/" target="_blank" rel="noreferrer"><img src={currencyApp} alt="" className="workImg"/></a><span className='pTitle'>Currency Converter</span></div>
+            <div className='pFrame'> <a href="https://visionary-cajeta-31f4df.netlify.app/" target="_blank" rel="noreferrer"><img src={todoApp} alt="" className="workImg"/></a><span className='pTitle'>Todo App</span></div>
             </div>
             <button className="workBtn">See More</button>
         </section>
