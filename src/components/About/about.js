e755e3cd5d6e22b0f9035218c512742d5b947341
Sittara22 <sittarashafique33@gmail.com>
2024-05-13 01:51:19 +0100

    Skill Added

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -5,6 +5,7 @@ import CSS from "../../assets/css3-alt.png";
 import HTML from "../../assets/html5.png";
 import react from "../../assets/react.png";
 import git from "../../assets/git-alt.png";
+import tailwind from "../../assets/tailwind.png";
 function About() {
   return (
     <div>
@@ -34,31 +35,38 @@ function About() {
             Hi,here are the set of skills that helps me to develope a website.{" "}
           </span>
           <div className="skillsIcon">
-            <div>
+            <div className="IconDiv">
               <span className="imgFrame">
                 <img src={js} alt="" className="Icon" />
               </span>
               <h2 className="text">Javascript</h2>
             </div>
-            <div>
+            <div className="IconDiv">
               {" "}
               <span className="imgFrame">
                 <img src={CSS} alt="" className="Icon" />
               </span>
               <h2 className="text">CSS</h2>
             </div>
-            <div>
+            <div className="IconDiv">
               <span className="imgFrame">
                 <img src={HTML} alt="" className="Icon" />
               </span>
               <h2 className="text">HTML</h2>
             </div>
-            <div>
+            <div className="IconDiv">
               <span className="imgFrame">
                 <img src={react} alt="" className="Icon" />
               </span>
               <h2 className="text">React</h2>
             </div>
+            <div className="IconDiv">
+              <span className="imgFrame">
+                <img src={tailwind} alt="" className="Icon" />
+              </span>
+              <h2 className="text">Tailwind CSS</h2>
+            </div>
+
             <div>
               <span className="imgFrame">
                 <img src={git} alt="" className="Icon" />
